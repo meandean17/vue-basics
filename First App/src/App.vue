95d4421d3e622a0bccb44f9ab1c8909f1f19881f
dean <deenomio@gmail.com>
2024-12-30 10:09:57 +0200

    first-app created, loading posts through an api

---
+++ First App/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import PostList from './components/PostList.vue';
+
+</script>
+
+<template>
+  <header>
+    <h1>Vue Basics - Async Data</h1>>
+  </header>
+  <main>
+    <PostList />
+  </main>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+</style>
